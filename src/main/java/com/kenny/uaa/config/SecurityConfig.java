ba1c4181aad2ec72ea4e08d3abcf0c907c9cf680
kenny <kenny.kwyiu@gmail.com>
2024-12-01 01:03:18 +0800

    config SecurityConfig

--- src/main/java/com/kenny/uaa/config/SecurityConfig.java
+++ src/main/java/com/kenny/uaa/config/SecurityConfig.java
@@ -1,12 +1,14 @@
 package com.kenny.uaa.config;
 
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
-@EnableWebSecurity
+@EnableWebSecurity(debug = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 //    @Override
@@ -25,9 +27,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
-                .httpBasic(Customizer.withDefaults())
-                .formLogin(form -> form.loginPage("/"))
-                .authorizeRequests(req -> req.antMatchers("/api/**").authenticated());
+        http
+            .authorizeRequests(req -> req.antMatchers("/api/**").authenticated())
+            .formLogin(form -> form.loginPage("/login"))
+            .httpBasic(Customizer.withDefaults())
+            .csrf(AbstractHttpConfigurer::disable)
+            ;
+    }
+
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        super.configure(auth);
+//    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().mvcMatchers("/public/**");
     }
 }
