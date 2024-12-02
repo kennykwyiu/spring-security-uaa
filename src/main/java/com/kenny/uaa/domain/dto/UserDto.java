adb360a6b5c6b193855322b4831bdc8ff06ed285
kenny <kenny.kwyiu@gmail.com>
2024-12-02 22:23:55 +0800

    add UserDto

---
+++ src/main/java/com/kenny/uaa/domain/dto/UserDto.java
@@ -0,0 +1,34 @@
+package com.kenny.uaa.domain.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import java.io.Serializable;
+@Data
+public class UserDto implements Serializable {
+    @NotNull
+    @NotBlank
+    @Size(min = 4, max = 50, message = "Username must be between 4 and 50 characters")
+    private String username;
+
+    @NotNull
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    private String password;
+
+    @NotNull
+    @Size(min = 8, message = "Matching password must be at least 8 characters")
+    private String matchingPassword;
+
+    @NotNull
+    @Email
+    private String email;
+
+    @NotNull
+    @NotBlank
+    @Size(min = 4, max = 50, message = "Name must be between 4 and 50 characters")
+    private String name;
+}
