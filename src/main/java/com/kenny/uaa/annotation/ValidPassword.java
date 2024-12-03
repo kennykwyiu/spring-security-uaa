ba4243ca72c79a36eeec6804cc4e78946af21fd0
kenny <kenny.kwyiu@gmail.com>
2024-12-04 00:06:34 +0800

    add ValidPassword annotation

---
+++ src/main/java/com/kenny/uaa/annotation/ValidPassword.java
@@ -0,0 +1,17 @@
+package com.kenny.uaa.annotation;
+
+import com.kenny.uaa.validation.PasswordConstraintValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Target({ElementType.TYPE, ElementType.FIELD, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = PasswordConstraintValidator.class)
+@Documented
+public @interface ValidPassword {
+    String message() default "Invalid Password";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
