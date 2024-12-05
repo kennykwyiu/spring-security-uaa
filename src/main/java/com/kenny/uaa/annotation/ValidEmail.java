2519ade9a3636185efed8f262607e590e6771d48
kenny <kenny.kwyiu@gmail.com>
2024-12-05 21:55:29 +0800

    revise ValidEmail

--- src/main/java/com/kenny/uaa/annotation/ValidEmail.java
+++ src/main/java/com/kenny/uaa/annotation/ValidEmail.java
@@ -11,7 +11,7 @@ import java.lang.annotation.*;
 @Constraint(validatedBy = EmailValidator.class)
 @Documented
 public @interface ValidEmail {
-    String message() default "Invalid Email";
+    String message() default "{ValidEmail.email}";
     Class<?>[] groups() default {};
     Class<? extends Payload>[] payload() default {};
 }
