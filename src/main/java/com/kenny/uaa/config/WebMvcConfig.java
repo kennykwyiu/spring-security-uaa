91e78587ebf9ce6ad22a48fcabff975ede80dfd9
kenny <kenny.kwyiu@gmail.com>
2024-12-06 21:18:43 +0800

    revise WebMvcConfig

--- src/main/java/com/kenny/uaa/config/WebMvcConfig.java
+++ src/main/java/com/kenny/uaa/config/WebMvcConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -18,6 +19,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Bean
     public MessageResolver messageResolver() {
         return new SpringMessageResolver(messageSource);
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean localValidatorFactoryBean() {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource);
+    return bean;
     }
 
     @Override
