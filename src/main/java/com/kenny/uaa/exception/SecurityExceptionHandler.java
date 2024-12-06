fe9e7d411ed7c0d90e81dda38a7df3d56f4c23ea
kenny <kenny.kwyiu@gmail.com>
2024-12-06 21:15:14 +0800

    add SecurityExceptionHandler

---
+++ src/main/java/com/kenny/uaa/exception/SecurityExceptionHandler.java
@@ -0,0 +1,6 @@
+package com.kenny.uaa.exception;
+
+import org.zalando.problem.spring.web.advice.security.SecurityAdviceTrait;
+
+public class SecurityExceptionHandler implements SecurityAdviceTrait  {
+}
