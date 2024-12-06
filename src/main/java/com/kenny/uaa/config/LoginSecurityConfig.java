7596033b4c287dfc506897981d2de90bfb2a2068
kenny <kenny.kwyiu@gmail.com>
2024-12-06 21:16:57 +0800

    add LoginSecurityConfig

---
+++ src/main/java/com/kenny/uaa/config/LoginSecurityConfig.java
@@ -0,0 +1,39 @@
+package com.kenny.uaa.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+@Configuration
+@Order(100)
+public class LoginSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(req -> req.anyRequest().authenticated())
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .usernameParameter("username1")
+                        .defaultSuccessUrl("/")
+//                        .successHandler(getJsonAuthenticationSuccessHandler())
+//                        .failureHandler(getJsonLoginFailureHandler())
+                        .permitAll())
+
+                .logout(logout -> logout
+                        .logoutUrl("/perform_logout"))
+//                        .logoutSuccessHandler(getJsonLogoutSuccessHandler()))
+                .rememberMe(rememberMe -> rememberMe
+                        .tokenValiditySeconds(30 * 24 * 3600) // 30days
+                        .rememberMeCookieName("someKeyToRemember"))
+                ;
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring().antMatchers("/public/**", "/error")
+                ;
+//                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+}
