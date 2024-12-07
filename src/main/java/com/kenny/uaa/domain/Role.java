72a64fd994e7c0ad76bb63064c1e8cdaef047d4b
kenny <kenny.kwyiu@gmail.com>
2024-12-08 00:34:03 +0800

    add Role

---
+++ src/main/java/com/kenny/uaa/domain/Role.java
@@ -0,0 +1,38 @@
+package com.kenny.uaa.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+/**
+ * Role entity class, implementing the GrantedAuthority interface
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "uaa_roles")
+public class Role implements GrantedAuthority, Serializable {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Auto-increment ID, unique identifier
+     */
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    /**
+     * Role name with unique constraint, cannot be duplicated
+     */
+    @NotNull
+    @Size(max = 50)
+    @Column(name = "role_name", unique = true, nullable = false, length = 50)
+    private String authority;
+}
