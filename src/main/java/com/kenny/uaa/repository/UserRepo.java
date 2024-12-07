e55f4620d2843488d10c1ca2621b7ed9b3a542de
kenny <kenny.kwyiu@gmail.com>
2024-12-08 00:35:19 +0800

    add UserRepo

---
+++ src/main/java/com/kenny/uaa/repository/UserRepo.java
@@ -0,0 +1,11 @@
+package com.kenny.uaa.repository;
+
+import com.kenny.uaa.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+@Repository
+public interface UserRepo extends JpaRepository<User, Long> {
+   Optional<User> findOptionalByUsername(String username);
+}
