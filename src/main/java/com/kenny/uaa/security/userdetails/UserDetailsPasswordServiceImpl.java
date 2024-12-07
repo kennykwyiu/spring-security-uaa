6cf1ffedf4a2001144caad80c40f9d022a1ee580
kenny <kenny.kwyiu@gmail.com>
2024-12-08 00:36:42 +0800

    add UserDetailsPasswordServiceImpl

---
+++ src/main/java/com/kenny/uaa/security/userdetails/UserDetailsPasswordServiceImpl.java
@@ -0,0 +1,20 @@
+package com.kenny.uaa.security.userdetails;
+
+import com.kenny.uaa.repository.UserRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsPasswordService;
+import org.springframework.stereotype.Service;
+@RequiredArgsConstructor
+@Service
+public class UserDetailsPasswordServiceImpl implements UserDetailsPasswordService {
+    private final UserRepo userRepo;
+    @Override
+    public UserDetails updatePassword(UserDetails userDetails, String newPassword) {
+        return userRepo.findOptionalByUsername(userDetails.getUsername())
+                .map(user -> (UserDetails) userRepo.save(user.withPassword(newPassword)))
+                .orElse(userDetails);
+
+
+    }
+}
