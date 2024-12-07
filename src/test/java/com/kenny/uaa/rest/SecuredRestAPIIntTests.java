12e19ecef9c3ad1cca208c51d1ff1c587f475fd4
kenny <kenny.kwyiu@gmail.com>
2024-12-08 00:41:46 +0800

    add SecuredRestAPIIntTests

---
+++ src/test/java/com/kenny/uaa/rest/SecuredRestAPIIntTests.java
@@ -0,0 +1,38 @@
+package com.kenny.uaa.rest;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+public class SecuredRestAPIIntTests {
+    @Autowired
+    private WebApplicationContext context;
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @WithMockUser
+    @Test
+    public void givenAuthRequest_shouldSucceedWith200() throws Exception {
+        mockMvc.perform(
+                get("/api/greeting")
+        )
+                .andExpect(status().isOk());
+    }
+}
