bc45ad5cbbeeb4838499984fd697b068eacf68f1
kenny <kenny.kwyiu@gmail.com>
2024-12-09 00:03:09 +0800

    revise LoginSecurityConfig

--- src/main/java/com/kenny/uaa/config/LoginSecurityConfig.java
+++ src/main/java/com/kenny/uaa/config/LoginSecurityConfig.java
@@ -1,14 +1,21 @@
 package com.kenny.uaa.config;
 
+import com.kenny.uaa.security.auth.ldap.LDAPMultiAuthenticationProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
+@RequiredArgsConstructor
 @Configuration
 @Order(100)
 public class LoginSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final DaoAuthenticationProvider daoAuthenticationProvider;
+    private final LDAPMultiAuthenticationProvider ldapMultiAuthenticationProvider;
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -36,5 +43,11 @@ public class LoginSecurityConfig extends WebSecurityConfigurerAdapter {
                 .ignoring().antMatchers("/public/**", "/error", "/h2-console/**")
                 ;
 //                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(ldapMultiAuthenticationProvider);
+        auth.authenticationProvider(daoAuthenticationProvider);
     }
 }
