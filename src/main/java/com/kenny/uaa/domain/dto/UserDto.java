78db3d48342b4c17886905529f1f28cd70b98f3f
kenny <kenny.kwyiu@gmail.com>
2024-12-10 21:05:54 +0800

    revise UserDto

--- src/main/java/com/kenny/uaa/domain/dto/UserDto.java
+++ src/main/java/com/kenny/uaa/domain/dto/UserDto.java
@@ -3,6 +3,7 @@ package com.kenny.uaa.domain.dto;
 import com.kenny.uaa.annotation.PasswordMatch;
 import com.kenny.uaa.annotation.ValidEmail;
 import com.kenny.uaa.annotation.ValidPassword;
+import com.kenny.uaa.util.Constants;
 import lombok.Data;
 
 import javax.validation.constraints.*;
@@ -32,4 +33,8 @@ public class UserDto implements Serializable {
     @NotBlank
     @Size(min = 4, max = 50, message = "Name must be between 4 and 50 characters")
     private String name;
+
+    @Pattern(regexp = Constants.PATTERN_MOBILE)
+    @NotNull
+    private String mobile;
 }
