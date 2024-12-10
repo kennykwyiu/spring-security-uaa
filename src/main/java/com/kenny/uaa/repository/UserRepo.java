2ed5789d2cc1bd2b5263b500a86f2e9cfbc9c63c
kenny <kenny.kwyiu@gmail.com>
2024-12-10 21:06:11 +0800

    revise UserRepo

--- src/main/java/com/kenny/uaa/repository/UserRepo.java
+++ src/main/java/com/kenny/uaa/repository/UserRepo.java
@@ -8,4 +8,7 @@ import java.util.Optional;
 @Repository
 public interface UserRepo extends JpaRepository<User, Long> {
    Optional<User> findOptionalByUsername(String username);
+   long countByUsername(String username);
+   long countByEmail(String email);
+   long countByMobile(String mobile);
 }
