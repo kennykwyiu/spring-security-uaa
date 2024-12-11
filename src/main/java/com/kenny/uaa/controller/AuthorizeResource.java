0475965a5f1bacf3871bbb79b4484b0578eac80a
kenny <kenny.kwyiu@gmail.com>
2024-12-11 22:39:31 +0800

    add register in AuthorizeResource

--- src/main/java/com/kenny/uaa/controller/AuthorizeResource.java
+++ src/main/java/com/kenny/uaa/controller/AuthorizeResource.java
@@ -5,6 +5,10 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.security.Key;
+import java.util.Optional;
+
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/authorize")
 public class AuthorizeResource {
@@ -14,8 +18,32 @@ public class AuthorizeResource {
     }
 
     @PostMapping("/register")
-    public UserDto register(@Valid @RequestBody UserDto userDto) {
-        return userDto;
+    public void register(@Valid @RequestBody UserDto userDto) {
+
+        // TODO: Verify that username, email, and mobile are unique - need to query database to check for duplicates
+        if (userService.isUsernameExisted(userDto.getUsername())) {
+            throw new DuplicateProblem("Username already exists");
+        }
+        if (userService.isEmailExisted(userDto.getEmail())) {
+            throw new DuplicateProblem("Email already exists");
+        }
+
+        if (userService.isMobileExisted(userDto.getMobile())) {
+            throw new DuplicateProblem("Mobile already exists");
+        }
+
+        User user = User.builder()
+                .username(userDto.getUsername())
+                .name(userDto.getName())
+                .email(userDto.getEmail())
+                .mobile(userDto.getMobile())
+                .password(userDto.getPassword())
+                .build();
+        // TODO: Convert userDto to User entity, assign default ROLE_USER role and save to database
+        userService.register(user);
+
+
+
     }
 
     @GetMapping("/problem")
