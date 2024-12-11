76a7622162e632381e1ff3896c7fc17ece8a47b6
kenny <kenny.kwyiu@gmail.com>
2024-12-11 22:36:17 +0800

    add BaseIntegrationTest

---
+++ src/test/java/com/kenny/uaa/common/BaseIntegrationTest.java
@@ -0,0 +1,11 @@
+package com.kenny.uaa.common;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class BaseIntegrationTest {
+}
