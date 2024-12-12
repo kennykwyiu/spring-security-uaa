911dca955a3f06fe5e149ee7f45f1791613d0d45
kenny <kenny.kwyiu@gmail.com>
2024-12-12 20:24:05 +0800

    revise

--- src/main/java/com/kenny/uaa/config/AppProperties.java
+++ src/main/java/com/kenny/uaa/config/AppProperties.java
@@ -4,6 +4,9 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+
+import javax.validation.Valid;
+
 //kenny.jwt.accessTokenExpireTime
 @ConfigurationProperties(prefix = "kenny")
 @Configuration
@@ -12,6 +15,24 @@ public class AppProperties {
     @Getter
     @Setter
     private Jwt jwt = new Jwt();
+
+    @Getter
+    @Setter
+    private Ali ali = new Ali();
+
+    @Getter
+    @Setter
+    @Valid
+    private LeanCloud leanCloud = new LeanCloud();
+
+    @Getter
+    @Setter
+    private SmsProvider smsProvider = new SmsProvider();
+
+    @Getter
+    @Setter
+    @Valid
+    private EmailProvider emailProvider = new EmailProvider();
 
     @Getter
     @Setter
