67a39e9e19152c793cc1b140a02b24d101f55862
kenny <kenny.kwyiu@gmail.com>
2024-12-15 14:37:01 +0800

    revise

--- src/main/java/com/kenny/uaa/util/Constants.java
+++ src/main/java/com/kenny/uaa/util/Constants.java
@@ -2,8 +2,29 @@ package com.kenny.uaa.util;
 
 public class Constants {
     public static final String PROBLEM_BASE_URI = "https://kenny.com";
-    public static final String PATTERN_MOBILE = "^(13[0-9]|14[579]|15[0-3,5-9]|16[6]|17[0135678]|18[0-9]|19[89])\\d{8}$";
-    public static final String ROLE_USER = "ROLE_USER";
-    public static final String ROLE_ADMIN = "ROLE_ADMIN";
+
+    // ---- Regular Expression related ----
+    public static final String PATTERN_MOBILE = "^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199|(147))\\d{8}$";
+    public static final String PATTERN_USERNAME = "^[a-z0-9_-]{3,50}$";
+    public static final String PATTERN_ROLE_NAME = "^[a-zA-Z0-9_]{3,50}$";
+
+    // ---- Authorization related ----
+    public static final String ROLE_PREFIX = "ROLE_";
+    public static final String AUTHORITY_USER = "USER";
+    public static final String AUTHORITY_STAFF = "STAFF";
+    public static final String AUTHORITY_MANAGER = "MANAGER";
+    public static final String AUTHORITY_ADMIN = "ADMIN";
+    public static final String ROLE_USER = ROLE_PREFIX + AUTHORITY_USER;
+    public static final String ROLE_STAFF = ROLE_PREFIX + AUTHORITY_STAFF;
+    public static final String ROLE_MANAGER = ROLE_PREFIX + AUTHORITY_MANAGER;
+    public static final String ROLE_ADMIN = ROLE_PREFIX + AUTHORITY_ADMIN;
+    public static final String AUTHORITY_USER_ADMIN = "USER_ADMIN";
+    public static final String AUTHORITY_USER_UPDATE = "USER_UPDATE";
+    public static final String AUTHORITY_USER_CREATE = "USER_CREATE";
+    public static final String AUTHORITY_USER_READ = "USER_READ";
+
+    // ---- Cache related ----
     public static final String CACHE_MFA = "cacheMfa";
+    public static final String CACHE_ALL_PERMISSIONS = "cacheAllPermissions";
+    public static final String CACHE_ALL_ROLES = "cacheAllRoles";
 }
