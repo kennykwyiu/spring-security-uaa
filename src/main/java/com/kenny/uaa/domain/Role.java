4458fae776df562c5aa19df242f45fb57963173d
kenny <kenny.kwyiu@gmail.com>
2024-12-16 21:49:25 +0800

    revise Role

--- src/main/java/com/kenny/uaa/domain/Role.java
+++ src/main/java/com/kenny/uaa/domain/Role.java
@@ -1,15 +1,20 @@
 package com.kenny.uaa.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 import org.springframework.security.core.GrantedAuthority;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Role entity class, implementing the GrantedAuthority interface
@@ -20,14 +25,14 @@ import java.io.Serializable;
 @Data
 @Entity
 @Table(name = "uaa_roles")
-public class Role implements GrantedAuthority, Serializable {
+public class Role implements Serializable {
     private static final long serialVersionUID = 1L;
 
     /**
      * Auto-increment ID, unique identifier
      */
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     /**
@@ -36,5 +41,18 @@ public class Role implements GrantedAuthority, Serializable {
     @NotNull
     @Size(max = 50)
     @Column(name = "role_name", unique = true, nullable = false, length = 50)
-    private String authority;
+    private String roleName;
+
+    @JsonIgnore
+    @Fetch(FetchMode.JOIN)
+    @ManyToMany
+    @JoinTable(name = "uaa_roles_permissions",
+            joinColumns = {@JoinColumn(name = "role_id", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "permission_id", referencedColumnName = "id")}
+    )
+    private Set<Permission> permissions = new HashSet<>();
+
+    @JsonIgnore
+    @ManyToMany(mappedBy = "roles")
+    private Set<Role> users;
 }
