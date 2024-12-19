6d41293dad324cce98b952e26f44a8f3117d01b0
kenny <kenny.kwyiu@gmail.com>
2024-12-19 21:29:29 +0800

    add SendTotpDto

---
+++ src/main/java/com/kenny/uaa/domain/dto/SendTotpDto.java
@@ -0,0 +1,23 @@
+package com.kenny.uaa.domain.dto;
+
+import com.kenny.uaa.domain.MfaType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SendTotpDto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @NotNull
+    private MfaType mfaType = MfaType.SMS;
+
+    @NotNull
+    private String mfaId;
+}
